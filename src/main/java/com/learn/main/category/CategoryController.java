d14701f7112e20c67b3ad0e8f0c66793cf6ab85a
mrtripop <tripop.chai12@gmail.com>
2023-06-24 14:48:48 +0700

    feat(category): get category and create new category
    Create CRUD, detail
    - Get all category
    - Get category by id
    - Create new category
    
    update: category service

--- src/main/java/com/learn/main/category/CategoryController.java
+++ src/main/java/com/learn/main/category/CategoryController.java
@@ -1,21 +1,64 @@
 package com.learn.main.category;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.learn.main.address.Address;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
 @RestController
-@RequestMapping("/api/inventory/category")
+@RequestMapping("/api/inventory/categories")
 public class CategoryController {
 
-    private final CategoryService categoryService;
+  private final CategoryService categoryService;
 
-    public CategoryController(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
+  public CategoryController(CategoryService categoryService) {
+    this.categoryService = categoryService;
+  }
 
-    @GetMapping
-    public String getCategory() {
-        return categoryService.getCategory();
+  @GetMapping
+  public ResponseEntity<List<Category>> getCategory() {
+    try {
+      List<Category> categories = categoryService.getCategory();
+      return new ResponseEntity<>(categories, HttpStatus.OK);
+    } catch (Exception e) {
+      String error = ExceptionUtils.getStackTrace(e);
+      log.error(error);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
+
+  @GetMapping("/{categoryId}")
+  public ResponseEntity<Category> retrieveCategoryById(
+      @PathVariable(name = "categoryId") Long categoryId) {
+    try {
+      Category categories = categoryService.getCategoryById(categoryId);
+      if (categories != null) {
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+      }
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    } catch (Exception e) {
+      String error = ExceptionUtils.getStackTrace(e);
+      log.error(error);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @PostMapping
+  public ResponseEntity<Category> createCategory(
+      @RequestBody Category newCategory,
+      @RequestParam(name = "categoryId", required = false) Long categoryId) {
+    try {
+      Category category = categoryService.createNewCategory(newCategory, categoryId);
+      return new ResponseEntity<>(category, HttpStatus.OK);
+    } catch (Exception e) {
+      String error = ExceptionUtils.getStackTrace(e);
+      log.error(error);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
