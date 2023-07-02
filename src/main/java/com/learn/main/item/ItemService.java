77f8c8cb2fe4fe82789d1022f0d2334a697827a4
mrtripop <tripop.chai12@gmail.com>
2023-07-02 22:24:29 +0700

    feat: add get all item method

--- src/main/java/com/learn/main/item/ItemService.java
+++ src/main/java/com/learn/main/item/ItemService.java
@@ -1,11 +1,33 @@
 package com.learn.main.item;
 
+import com.learn.helper.DatabaseHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
 public class ItemService {
 
-    public String getItems() {
-        return "Get items";
+  private final ItemRepository itemRepository;
+
+  public ItemService(ItemRepository itemRepository) {
+    this.itemRepository = itemRepository;
+  }
+
+  public List<Item> getItems(Integer page, Integer size, String orderBy) {
+    try {
+      Pageable pageSize = DatabaseHelper.initPageableWithSort(page, size, orderBy);
+      Page<Item> result = itemRepository.findAll(pageSize);
+      return result.getContent();
+    } catch (Exception e) {
+      String error = ExceptionUtils.getStackTrace(e);
+      log.error(error);
+      throw new RuntimeException("GetItemsException", e.getCause());
     }
+  }
 }
