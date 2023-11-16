c03ffcda70263b27aab948660c230db5a3fa0207
mrtripop <tripop.chai12@gmail.com>
2023-11-16 16:59:24 +0700

    feat: response and status model
    Author: mrtripop

---
+++ src/main/java/com/learn/model/StatusModel.java
@@ -0,0 +1,15 @@
+package com.learn.model;
+
+import lombok.Getter;
+
+@Getter
+public class StatusModel {
+
+  private final String code;
+  private final String message;
+
+  public StatusModel(String code, String message) {
+    this.code = code;
+    this.message = message;
+  }
+}
