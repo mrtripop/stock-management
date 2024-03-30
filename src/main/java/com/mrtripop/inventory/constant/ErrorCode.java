5216a655e815b8c3ce5b8b4de0bc0b819d83dba3
mrtripop <tripop.chai12@gmail.com>
2024-03-30 16:15:04 +0700

    feat: response model builder
    Author: mrtripop

--- src/main/java/com/mrtripop/inventory/constant/ErrorCode.java
+++ src/main/java/com/mrtripop/inventory/constant/ErrorCode.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
   CANNOT_GET_ALL_SONG_BY_INTERRUPT_EXCEPTION("SBI4001", "Cannot get song by interrupt exception"),
-  CANNOT_GET_ALL_SONG_EXCEPTION("SBE4002", "Cannot get song exception");
+  CANNOT_GET_ALL_SONG_EXCEPTION("SBE4002", "Cannot get song exception"),
+  USER5000_RETRIEVE_USERS_IS_FAILED("USER5000", "Cannot get users from database");
 
   private final String code;
   private final String message;
