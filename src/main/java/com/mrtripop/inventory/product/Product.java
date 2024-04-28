e75a3ecf9a1b952b11309f9289c7637c97c9eeeb
mrtripop <tripop.chai12@gmail.com>
2024-04-28 15:37:11 +0700

    feat: column name, data type, index, and event audit of product entity
    Author: mrtripop

--- src/main/java/com/mrtripop/inventory/product/Product.java
+++ src/main/java/com/mrtripop/inventory/product/Product.java
@@ -2,16 +2,27 @@ package com.mrtripop.inventory.product;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
-import java.time.ZonedDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Data
 @Entity
-@Table(name = "product")
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
+@Table(
+    name = "product",
+    indexes = {
+      @Index(name = "product_code", columnList = "code"),
+      @Index(name = "product_created_at", columnList = "created_at"),
+      @Index(name = "product_updated_at", columnList = "updated_at"),
+    })
+@EntityListeners(AuditingEntityListener.class)
 public class Product implements Serializable {
 
   @Id
@@ -19,21 +30,79 @@ public class Product implements Serializable {
   @SequenceGenerator(name = "product_seq", allocationSize = 1)
   private Long id;
 
-  @Column(columnDefinition = "smallint")
-  private Integer type;
+  /**
+   * ProductCode: Besides the ProductID, products are usually identified by an internal code (also
+   * called an SKU or Stock Keeping Unit). This code consists of letters and numbers that identify
+   * characteristics about each product, such as manufacturer, brand, style, color, and size.
+   */
+  @Column(name = "code", columnDefinition = "TEXT", nullable = false)
+  private String code;
 
-  @Column(columnDefinition = "varchar(75)")
-  private String title;
+  /**
+   * Barcode: This external product code (also known as the UPC or Universal Product Code) is
+   * standardized for universal use by any company.
+   */
+  @Column(name = "barcode", columnDefinition = "TEXT")
+  private String barcode;
 
-  @Column(columnDefinition = "text")
-  private String summary;
+  /** ProductName: The product's name. */
+  @Column(name = "name", columnDefinition = "TEXT")
+  private String name;
 
-  @Column(columnDefinition = "text")
-  private String content;
+  /** ProductDescription: A more detailed description of the product. */
+  @Column(name = "description", columnDefinition = "TEXT")
+  private String description;
 
-  @JdbcTypeCode(SqlTypes.TIMESTAMP_WITH_TIMEZONE)
-  private ZonedDateTime createdAt;
+  /** ProductCategory: The product’s category and should be normalized. */
+  @Column(name = "category", columnDefinition = "TEXT")
+  private String category;
 
-  @JdbcTypeCode(SqlTypes.TIMESTAMP_WITH_TIMEZONE)
-  private ZonedDateTime updatedAt;
+  /**
+   * ReorderQuantity: Some products cannot be ordered by units. You need to purchase them in
+   * packages.
+   */
+  @Column(name = "reorder_quantity", columnDefinition = "SMALLINT")
+  private Integer reorderQuantity;
+
+  /**
+   * PackedWeight: Product’s weight, including packaging. This may be required to define storage
+   * location.
+   */
+  @Column(name = "packed_weight", columnDefinition = "DECIMAL")
+  private Double packedWeight;
+
+  /**
+   * PackedWeight: Product’s height, including packaging. This may be required to define storage
+   * location.
+   */
+  @Column(name = "packed_height", columnDefinition = "DECIMAL")
+  private Double packedHeight;
+
+  /**
+   * PackedWeight: Product’s width, including packaging. This may be required to define storage
+   * location.
+   */
+  @Column(name = "packed_width", columnDefinition = "DECIMAL")
+  private Double packedWidth;
+
+  /**
+   * PackedWeight: Product’s depth, including packaging. This may be required to define storage
+   * location.
+   */
+  @Column(name = "packed_depth", columnDefinition = "DECIMAL")
+  private Double packedDepth;
+
+  /** IsActive: Is status of the product is selling or not */
+  @Column(name = "is_active", columnDefinition = "BOOLEAN")
+  private Boolean isActive;
+
+  /** CreatedAt: Time that product is created */
+  @CreatedDate
+  @Column(name = "created_at", columnDefinition = "BIGINT")
+  private Long createdAt;
+
+  /** UpdatedAt: Time that product is updated */
+  @LastModifiedDate
+  @Column(name = "updated_at", columnDefinition = "BIGINT")
+  private Long updatedAt;
 }
