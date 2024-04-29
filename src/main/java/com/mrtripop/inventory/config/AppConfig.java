db8df97880492c979c3179e90c293ab88833d69c
mrtripop <tripop.chai12@gmail.com>
2024-04-30 02:09:04 +0700

    feat: config spring AOP
    Author: mrtripop

---
+++ src/main/java/com/mrtripop/inventory/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.mrtripop.inventory.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.mrtripop.inventory.product.component")
+public class AppConfig {}
---
+++ src/main/java/com/mrtripop/inventory/product/component/ProductAspect.java
@@ -0,0 +1,8 @@
+package com.mrtripop.inventory.product.component;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ProductAspect {}
