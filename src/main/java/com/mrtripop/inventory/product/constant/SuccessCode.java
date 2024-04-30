27caac4192bf7e8bd9b0407f5a9997e0996fae28
mrtripop <tripop.chai12@gmail.com>
2024-05-01 02:43:23 +0700

    feat: add success code enum
    Author: mrtripop

--- src/main/java/com/mrtripop/inventory/product/constant/SuccessCode.java
+++ src/main/java/com/mrtripop/inventory/product/constant/SuccessCode.java
@@ -7,7 +7,8 @@ public enum SuccessCode implements BaseStatusCode {
   PRO2002_GET_PRODUCTS_BY_ID_IS_SUCCESS("PRO2002", "Get product by ID is success"),
   PRO2003_CREATE_NEW_PRODUCT_IS_SUCCESS("PRO2003", "Create new product is success"),
   PRO2004_UPDATE_PRODUCT_IS_SUCCESS("PRO2004", "Update the product is success"),
-  PRO2005_DELETE_PRODUCT_IS_SUCCESS("PRO2005", "Delete product is success");
+  PRO2005_DELETE_PRODUCT_IS_SUCCESS("PRO2005", "Delete product is success"),
+  PRO2006_GET_ALL_PRODUCT_HISTORIES_IS_SUCCESS("PRO2006", "Get all product histories is success");
   private final String code;
   private final String message;
 
