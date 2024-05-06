3b0a0c26a894036ce05f371166db0ee13dd583d0
mrtripop <tripop.chai12@gmail.com>
2024-05-06 17:25:09 +0700

    feat: config cache product data via cache abstraction
    Author: mrtripop

--- src/main/java/com/mrtripop/inventory/config/RedisConfig.java
+++ src/main/java/com/mrtripop/inventory/config/RedisConfig.java
@@ -2,18 +2,27 @@ package com.mrtripop.inventory.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
   @Bean
-  public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-    RedisTemplate<String, String> template = new RedisTemplate<>();
-    template.setConnectionFactory(connectionFactory);
-    template.setHashValueSerializer(new StringRedisSerializer());
-    return template;
+  public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+    return RedisCacheManager.builder(connectionFactory)
+        .cacheDefaults(
+            RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(
+                    RedisSerializationContext.SerializationPair.fromSerializer(
+                        new StringRedisSerializer()))
+                .serializeValuesWith(
+                    RedisSerializationContext.SerializationPair.fromSerializer(
+                        new GenericJackson2JsonRedisSerializer())))
+        .build();
   }
 }
