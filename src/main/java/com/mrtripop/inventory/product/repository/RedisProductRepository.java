37d2b04862a7cb38c8c1198f4386a97d03160bd4
mrtripop <tripop.chai12@gmail.com>
2024-05-06 17:30:15 +0700

    chore: remove unused product cache model and repository
    Author: mrtripop

--- src/main/java/com/mrtripop/inventory/product/repository/RedisProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mrtripop.inventory.product.repository;
-
-import com.mrtripop.inventory.product.models.ProductCache;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RedisProductRepository extends CrudRepository<ProductCache, Long> {}
