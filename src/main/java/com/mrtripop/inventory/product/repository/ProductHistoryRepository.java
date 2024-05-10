7c7f03fc715685e46fee94eb40c974d40b74ca8c
mrtripop <tripop.chai12@gmail.com>
2024-05-10 23:43:16 +0700

    refactor: structure product package model
    Author: mrtripop

--- src/main/java/com/mrtripop/inventory/product/repository/ProductHistoryRepository.java
+++ src/main/java/com/mrtripop/inventory/product/repository/ProductHistoryRepository.java
@@ -1,6 +1,6 @@
 package com.mrtripop.inventory.product.repository;
 
-import com.mrtripop.inventory.product.models.ProductHistory;
+import com.mrtripop.inventory.product.models.db.ProductHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/mrtripop/inventory/product/repository/ProductRepository.java
+++ src/main/java/com/mrtripop/inventory/product/repository/ProductRepository.java
@@ -1,6 +1,6 @@
 package com.mrtripop.inventory.product.repository;
 
-import com.mrtripop.inventory.product.models.Product;
+import com.mrtripop.inventory.product.models.db.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
