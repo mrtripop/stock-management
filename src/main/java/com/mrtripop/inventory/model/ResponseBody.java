360bcb5284ac75ed716e13bf4cdf39f3516ff239
mrtripop <tripop.chai12@gmail.com>
2024-05-15 19:43:17 +0700

    style: optimize import
    Author: mrtripop

--- src/main/java/com/mrtripop/inventory/model/ResponseBody.java
+++ src/main/java/com/mrtripop/inventory/model/ResponseBody.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
