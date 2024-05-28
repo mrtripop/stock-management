04802fd0295789542f9ee57968dc783806fbb3fe
mrtripop <tripop.chai12@gmail.com>
2024-05-28 21:23:04 +0700

    feat: add warehouse domain to relate with address domain
    Author: mrtripop

--- src/main/java/com/mrtripop/location/controllers/WarehouseController.java
+++ src/main/java/com/mrtripop/location/controllers/WarehouseController.java
@@ -1,10 +1,19 @@
 package com.mrtripop.location.controllers;
 
+import com.mrtripop.constant.BaseStatusCode;
+import com.mrtripop.exception.GlobalThrowable;
+import com.mrtripop.location.constant.ErrorCode;
+import com.mrtripop.location.constant.SuccessCode;
+import com.mrtripop.location.models.entities.Warehouse;
 import com.mrtripop.location.services.WarehouseService;
+import com.mrtripop.model.QueryParams;
+import com.mrtripop.model.ResponseBody;
+import jakarta.validation.Valid;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -17,7 +26,60 @@ public class WarehouseController {
     this.warehouseService = warehouseService;
   }
 
-  // admin permission
   @GetMapping
-  public void getAllWarehouses() {}
+  public ResponseEntity<Object> getWarehouses(@Valid QueryParams queryParams)
+      throws GlobalThrowable {
+    try {
+      List<Warehouse> warehouses = warehouseService.getWarehouses(queryParams);
+      BaseStatusCode status = SuccessCode.SUCCESS;
+      return ResponseBody.builder()
+          .code(status.getCode())
+          .message(status.getMessage())
+          .data(warehouses)
+          .build()
+          .buildResponseEntity(HttpStatus.OK);
+    } catch (Exception e) {
+      log.error("Cannot get all warehouse: {}", e.getMessage());
+      throw new GlobalThrowable(
+          ErrorCode.UAD5001_CANNOT_RETRIEVE_ADDRESSES, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @GetMapping("/{warehouse_id}")
+  public ResponseEntity<Object> getWarehouseByWarehouseId(
+      @PathVariable(name = "warehouse_id") Long warehouseId) throws GlobalThrowable {
+    try {
+      Warehouse warehouse = warehouseService.getWarehouseByWarehouseId(warehouseId);
+      BaseStatusCode status = SuccessCode.SUCCESS;
+      return ResponseBody.builder()
+          .code(status.getCode())
+          .message(status.getMessage())
+          .data(warehouse)
+          .build()
+          .buildResponseEntity(HttpStatus.OK);
+    } catch (Exception e) {
+      log.error("Cannot get warehouse by warehouse Id({}): {}", warehouseId, e.getMessage());
+      throw new GlobalThrowable(
+          ErrorCode.UAD5001_CANNOT_RETRIEVE_ADDRESSES, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @GetMapping("/addresses/{address_id}")
+  public ResponseEntity<Object> getWarehouseByAddressId(
+      @PathVariable(name = "address_id") Long addressId) throws GlobalThrowable {
+    try {
+      Warehouse warehouse = warehouseService.getWarehouseByAddressId(addressId);
+      BaseStatusCode status = SuccessCode.SUCCESS;
+      return ResponseBody.builder()
+          .code(status.getCode())
+          .message(status.getMessage())
+          .data(warehouse)
+          .build()
+          .buildResponseEntity(HttpStatus.OK);
+    } catch (Exception e) {
+      log.error("Cannot get warehouse by address Id({}): {}", addressId, e.getMessage());
+      throw new GlobalThrowable(
+          ErrorCode.UAD5001_CANNOT_RETRIEVE_ADDRESSES, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
--- src/main/java/com/mrtripop/location/repositories/WarehouseRepository.java
+++ src/main/java/com/mrtripop/location/repositories/WarehouseRepository.java
@@ -1,8 +1,12 @@
 package com.mrtripop.location.repositories;
 
-import com.mrtripop.location.models.Warehouse;
+import com.mrtripop.location.models.entities.Address;
+import com.mrtripop.location.models.entities.Warehouse;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {}
+public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
+  Optional<Warehouse> findByAddress(Address address);
+}
