2b1432355e2e67df52d052b3d58df35f8bb02eac
mrtripop <tripop.chai12@gmail.com>
2024-05-28 21:21:16 +0700

    feat: migrate address domain to use query param object and create db record
    Author: mrtripop

--- src/main/java/com/mrtripop/location/controllers/AddressController.java
+++ src/main/java/com/mrtripop/location/controllers/AddressController.java
@@ -1,17 +1,18 @@
 package com.mrtripop.location.controllers;
 
-import com.mrtripop.location.constant.ErrorCode;
-import com.mrtripop.location.constant.SuccessCode;
-import com.mrtripop.location.models.Address;
-import com.mrtripop.location.services.AddressService;
 import com.mrtripop.constant.BaseStatusCode;
 import com.mrtripop.exception.GlobalThrowable;
+import com.mrtripop.location.constant.ErrorCode;
+import com.mrtripop.location.constant.SuccessCode;
+import com.mrtripop.model.QueryParams;
+import com.mrtripop.location.models.dtos.AddressDTO;
+import com.mrtripop.location.models.entities.Address;
+import com.mrtripop.location.services.AddressService;
 import com.mrtripop.model.ResponseBody;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,22 +24,15 @@ public class AddressController {
 
   private final AddressService addressService;
 
-  @Autowired
   public AddressController(AddressService addressService) {
     this.addressService = addressService;
   }
 
-  // admin permission
   @GetMapping
-  public ResponseEntity<Object> getAddresses(
-      @RequestParam(defaultValue = "1") @Min(value = 1, message = "Page must not less than one")
-          Integer page,
-      @RequestParam(defaultValue = "200") @Min(value = 1, message = "Size must not less than one")
-          Integer size,
-      @RequestParam(defaultValue = "ASC") Sort.Direction orderBy)
+  public ResponseEntity<Object> getAddresses(@Valid QueryParams queryParams)
       throws GlobalThrowable {
     try {
-      List<Address> addresses = addressService.getAddresses(page, size, orderBy);
+      List<Address> addresses = addressService.getAddresses(queryParams);
       BaseStatusCode code = SuccessCode.SUCCESS;
       return ResponseBody.builder()
           .code(code.getCode())
@@ -53,7 +47,6 @@ public class AddressController {
     }
   }
 
-  // admin permission
   @GetMapping("/{addressId}")
   public ResponseEntity<Object> getAddressesById(
       @PathVariable @Min(value = 1, message = "Address ID is invalid") Long addressId)
@@ -69,6 +62,25 @@ public class AddressController {
           .buildResponseEntity(HttpStatus.OK);
     } catch (Exception e) {
       log.error("Cannot get addresses by ID '{}': {}", addressId, e.getMessage());
+      throw new GlobalThrowable(
+          ErrorCode.UAD5003_NOT_FOUND_ADDRESSES_FOR_ADDRESS_ID, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @PostMapping
+  public ResponseEntity<Object> addNewAddress(@RequestBody @Valid AddressDTO addressDTO)
+      throws GlobalThrowable {
+    try {
+      Address address = addressService.addNewAddress(addressDTO);
+      BaseStatusCode code = SuccessCode.SUCCESS;
+      return ResponseBody.builder()
+          .code(code.getCode())
+          .message(code.getMessage())
+          .data(address)
+          .build()
+          .buildResponseEntity(HttpStatus.OK);
+    } catch (Exception e) {
+      log.error("Cannot add new address: {}", e.getMessage());
       throw new GlobalThrowable(
           ErrorCode.UAD5003_NOT_FOUND_ADDRESSES_FOR_ADDRESS_ID, HttpStatus.INTERNAL_SERVER_ERROR);
     }
--- src/main/java/com/mrtripop/location/services/AddressService.java
+++ src/main/java/com/mrtripop/location/services/AddressService.java
@@ -2,13 +2,14 @@ package com.mrtripop.location.services;
 
 import com.mrtripop.exception.GlobalThrowable;
 import com.mrtripop.location.constant.ErrorCode;
-import com.mrtripop.location.models.Address;
+import com.mrtripop.location.models.dtos.AddressDTO;
+import com.mrtripop.location.models.entities.Address;
 import com.mrtripop.location.repositories.AddressRepository;
+import com.mrtripop.location.utils.AddressUtil;
+import com.mrtripop.model.QueryParams;
 import com.mrtripop.util.DatabaseHelper;
 import java.util.List;
-
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -21,19 +22,17 @@ public class AddressService {
 
   private final AddressRepository addressRepository;
 
-  @Autowired
   public AddressService(AddressRepository addressRepository) {
     this.addressRepository = addressRepository;
   }
 
-  public List<Address> getAddresses(Integer page, Integer size, Sort.Direction orderBy)
-      throws GlobalThrowable {
+  public List<Address> getAddresses(QueryParams queryParams) throws GlobalThrowable {
     try {
-      Pageable pageable = DatabaseHelper.initPageableWithSort(page, size, orderBy);
+      Pageable pageable = DatabaseHelper.initPageableWithSort(queryParams);
       Page<Address> pagesAddress = addressRepository.findAll(pageable);
       return pagesAddress.getContent();
     } catch (Exception e) {
-      log.error("Cannot retrieve addresses: {}", e.getMessage());
+      log.error("Cannot select addresses: {}", e.getMessage());
       throw new GlobalThrowable(
           ErrorCode.UAD5001_CANNOT_RETRIEVE_ADDRESSES, HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -48,10 +47,21 @@ public class AddressService {
                   new GlobalThrowable(
                       ErrorCode.UAD5003_NOT_FOUND_ADDRESSES_FOR_ADDRESS_ID, HttpStatus.NOT_FOUND));
     } catch (Exception e) {
-      log.error("Cannot get addresses by address ID '{}': {}", addressId, e.getMessage());
+      log.error("Cannot select addresses by address ID({}): {}", addressId, e.getMessage());
       throw new GlobalThrowable(
           ErrorCode.UAD5002_CANNOT_RETRIEVE_ADDRESSES_BY_ADDRESS_ID,
           HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  public Address addNewAddress(AddressDTO addressDTO) throws GlobalThrowable {
+    try {
+      Address address = AddressUtil.dtoToAddress(addressDTO);
+      return addressRepository.save(address);
+    } catch (Exception e) {
+      log.error("Cannot insert new address: {}", e.getMessage());
+      throw new GlobalThrowable(
+          ErrorCode.UAD5001_CANNOT_RETRIEVE_ADDRESSES, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 }
