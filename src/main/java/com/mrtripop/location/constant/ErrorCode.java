bbf9a90fe415fd978af3a9b018db20957d371a5f
mrtripop <tripop.chai12@gmail.com>
2024-05-30 23:00:13 +0700

    feat: update address error code
    Author: mrtripop

--- src/main/java/com/mrtripop/location/constant/ErrorCode.java
+++ src/main/java/com/mrtripop/location/constant/ErrorCode.java
@@ -6,7 +6,11 @@ public enum ErrorCode implements BaseStatusCode {
   UAD5001_CANNOT_RETRIEVE_ADDRESSES("UAD5001", "Cannot retrieve addresses"),
   UAD5002_CANNOT_RETRIEVE_ADDRESSES_BY_ADDRESS_ID(
       "UAD5002", "Cannot retrieve addresses by address ID"),
-  UAD5003_NOT_FOUND_ADDRESSES_FOR_ADDRESS_ID("UAD5003", "Not found addresses for the address ID");
+  UAD5003_NOT_FOUND_ADDRESSES_FOR_ADDRESS_ID("UAD5003", "Not found addresses for the address ID"),
+  UAD5004_CANNOT_UPDATE_THE_ADDRESS("UAD5004", "The controller cannot update the address"),
+  UAD5005_CANNOT_UPDATE_THE_EXISTING_ADDRESS_ID("UAD5005", "Cannot update the existing address ID"),
+  UAD5006_CANNOT_DELETE_THE_ADDRESS("UAD5006", "Cannot delete the address"),
+  UAD5007_CANNOT_DELETE_THE_ADDRESS_ID("UAD5007", "Cannot delete the address ID");
 
   private final String code;
   private final String message;
